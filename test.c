734f68deebb6e3867cbe701bc810ba2eb75e3a8b
TEJA_1290 <39546736+saiteja1290@users.noreply.github.com>
2022-03-06 17:08:39 +0530

    Add files via upload

--- test.c
+++ test.c
@@ -1,13 +1,49 @@
-#include<stdio.h>
-
-int main(int argc, char const *argv[])
+#include <stdio.h>
+#include <math.h>
+void main()
 {
-    int i,fact;
-    for (i = 0; i <= 10; i++)
+    int a[100], x, r, n, z, k, y, i, j, q, t;
+    printf("Enter Number:\n");
+    scanf("%d", &n);
+
+    // To Find Number Of Digits
+    // y = no of digits
+    for (i = 1; i < 100; i++)
     {
-        /* code */
-        fact = i+1;
+        x = pow(10, i);
+        if (n / x == 0)
+        {
+            y = i;
+            break;
+        }
     }
-    printf("%d",fact);
-    return 0;
+    // printf("Number of Digits:%d\n",y);
+    // storing the digits in array
+    for (i = y - 1; i >= 0; i--)
+    {
+        z = pow(10, i);
+        k = n / z;
+        a[y - 1 - i] = k;
+        n = n - k * z;
+    }
+
+    printf("Enter Number of Rotations:\n");
+    scanf("%d", &r);
+    while (r>0)
+    {
+        t = a[0];
+        a[0] = a[y - 1];
+
+        for (i = 0; i <= y-3; i++)
+        {
+            a[y - i - 1] = a[y - i - 2];
+        }
+        a[1] = t;
+        r = r - 1;
+    }
+
+    for (k = 0; k < y; k++)
+    {
+        printf("%d", a[k]);
+    }
 }
